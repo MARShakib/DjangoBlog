76a425e4d09a80686fa2248e75daa31e1f2a75ca
Md Ashikur Rahman <arshakib51@gmail.com>
2024-04-10 12:51:49 +0600

    add add_post.html that allows to post from frontend

--- ablog/theblog/urls.py
+++ ablog/theblog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     # path("", views.home, name="home"),
     path("", views.HomeView.as_view(), name="home"),
     path("article/<int:pk>", views.ArticleDetailView.as_view(), name="article-detail"),
+    path("add_post/", views.AddPostView.as_view(), name="add_post"),
 ]
